9796e5115624c96fda8fd8a66120de93a8f9ee33
Iain Matcham <iain@jumbletree.com>
2018-02-02 13:46:21 +1300

    Loading spinner update

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Loading = (props) => {
   return (
   	<div id="loading">
-		<i className="fa fa-cog fa-spin fa-3x fa-fw"></i>
+		<i className="fas fa-circle-notch fa-spin fa-3x fa-fw"></i>
 		<span className="sr-only">Loading...</span>
   	</div>
   );
