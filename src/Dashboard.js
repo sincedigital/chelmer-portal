900dd4441253e8a5421199a7da3ac3ddf05738ae
Iain Matcham <iain@jumbletree.com>
2018-02-05 16:52:01 +1300

    Move holding manipulation to a function

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,59 +1,189 @@
 import React, { Component } from 'react';
+
+import './App.css';
+
+import DateFormat from './components/DateFormat.js';
+import NumberFormat from './components/NumberFormat.js';
+
+import Modal from './components/Modal.js';
 import Navigation from './components/Navigation.js';
 import Loading from './components/Loading.js';
 import Footer from './components/Footer.js';
-import './App.css';
+import Remote from './components/Remote.js';
+import { Portfolios } from './components/Constants.js';
+import { ToAPIDate, PortfolioFromHoldings } from './components/Functions.js';
+
+import './Dashboard.css';
 
 class Dashboard extends Component {
 
+	state = {
+		loading: true,
+		portfolio: {
+			date: new Date(),
+			parts:[]
+		},
+		mtd: {
+			unloaded: true
+		},
+		ytd: {
+			unloaded: true
+		},
+		timeout: false
+	};
+	
+	constructor(props) {
+		super(props);
+		
+		this.load = this.load.bind(this);
+		
+		this.acceptPortfolio = this.acceptPortfolio.bind(this);
+		this.acceptMTDPortfolio = this.acceptMTDPortfolio.bind(this);
+		this.acceptYTDPortfolio = this.acceptYTDPortfolio.bind(this);
+		this.acceptMTDPerformance = this.acceptMTDPerformance.bind(this);
+		this.acceptYTDPerformance = this.acceptYTDPerformance.bind(this);
+		
+		this.load();
+	}
+
+	load() {
+		const now = new Date();
+		
+		Remote.getHoldings(
+			//TODO make portfolio adjustable
+			Portfolios[0].portfolio, 
+			ToAPIDate(now),
+			this.acceptPortfolio,
+			()=>{this.setState({loginRequired: true})},
+			()=>{this.setState({"timeout": true})}
+		);
+		
+		const month = new Date();
+		month.setDate(1);
+		
+		Remote.getHoldings(
+			//TODO make portfolio adjustable
+			Portfolios[0].portfolio, 
+			ToAPIDate(month),
+			this.acceptMTDPortfolio,
+			()=>{this.setState({loginRequired: true})},
+			()=>{this.setState({"timeout": true})}
+		);
+
+		Remote.getPerformance(
+			Portfolios[0].portfolio, 
+			ToAPIDate(month),
+			ToAPIDate(now),
+			this.acceptMTDPerformance,
+			()=>{this.setState({loginRequired: true})},
+			()=>{this.setState({"timeout": true})}
+		);
+		
+		const year = new Date();
+		year.setMonth(0);
+		year.setDate(1);
+		
+		Remote.getHoldings(
+			//TODO make portfolio adjustable
+			Portfolios[0].portfolio, 
+			ToAPIDate(year),
+			this.acceptYTDPortfolio,
+			()=>{this.setState({loginRequired: true})},
+			()=>{this.setState({"timeout": true})}
+		);
+
+		Remote.getPerformance(
+			Portfolios[0].portfolio, 
+			ToAPIDate(year),
+			ToAPIDate(now),
+			this.acceptYTDPerformance,
+			()=>{this.setState({loginRequired: true})},
+			()=>{this.setState({"timeout": true})}
+		);
+
+		
+	}
+	
+	acceptPortfolio(data) {
+		this.setState({"portfolio": PortfolioFromHoldings(data), "loading": false, "timeout": false});
+	}
+
+	acceptMTDPortfolio(data) {
+		this.setState({"mtd": PortfolioFromHoldings(data)});
+	}
+
+	acceptYTDPortfolio(data) {
+		this.setState({"ytd": PortfolioFromHoldings(data)});
+	}
+
+	acceptMTDPerformance(data) {
+		console.log("Month:")
+		console.log(data);
+	}
+	
+	acceptYTDPerformance(data) {
+		console.log("Year:")
+		console.log(data);
+	}
+	
   render() {
-    return (
+	  const wait = (<i className="fas fa-circle-notch fa-spin"></i>);
+	  
+	  var thisMonthProfitLoss = "";
+	  var thisMonthPercentage = "";
+	  var thisYearProfitLoss = "";
+	  var thisYearPercentage = "";
+	  
+	  if (this.state.mtd.unloaded) {
+		  thisMonthProfitLoss = wait;
+		  thisMonthPercentage = wait;
+	  } else {
+		  thisMonthProfitLoss = (<NumberFormat value={this.state.portfolio.total - this.state.mtd.total} places={2} prefix="$" />);
+		  thisMonthPercentage = (<NumberFormat value={(this.state.portfolio.total - this.state.mtd.total)/this.state.mtd.total*100} places={2} suffix="%" />);
+	  }
+	  
+	  if (this.state.ytd.unloaded) {
+		  thisYearProfitLoss = wait;
+		  thisYearPercentage = wait;
+	  } else {
+		  thisYearProfitLoss = (<NumberFormat value={this.state.portfolio.total - this.state.ytd.total} places={2} prefix="$" />);
+		  thisYearPercentage = (<NumberFormat value={(this.state.portfolio.total - this.state.ytd.total)/this.state.ytd.total*100} places={2} suffix="%" />);
+	  }
+
+	 return (
       <div className="App">
         <Navigation url={this.props.match.url} />
         <div className="main-content-section">
-        	<Loading />
-            <div className="hero-wrap">
-            <div className="main-content">
-              <div className="div-block w-clearfix">
-              </div>
+        	{ this.state.loading === true && this.state.timeout === false ? <Loading /> : null }
+           	<div className="hero-wrap">
+            	<div className="main-content">
+            		<div className="div-block w-clearfix">
+            		</div>
+            	</div>
             </div>
-          </div>
-          <div id="dashboard" >
-          <div className="w-col w-col-6" >
-          	<div className=" w-col-dashboard">
-              	<h1>NET WORTH <span className="right">TODAY</span></h1>
-              	<h1 className="main">$1,686,915</h1>
-              	<div className="row row-positive">
-              	<div className="row-date">This month</div>
-              	<div className="row-amount">+$694,755</div>
-              	<div className="row-percent">+70.02%</div>
-              	</div>
-              	<div className="row row-negative">
-              	<div className="row-date">YEAR TO DATE</div>
-              	<div className="row-amount">-$50,755</div>
-              	<div className="row-percent">-5.02%</div>
-              	</div>
-              	</div>
-              	<div className=" w-col-dashboard">
-              	<h1>INVESTMENTS<span className="right">TODAY</span></h1>
-              	<p>Asset allocation</p>
-              	
-              	<canvas id="assetAllocation" width="300" height="150"></canvas>
-              	</div>
-              	</div>
-              	<div className="w-col w-col-6 ">
-              	<div className="w-col-dashboard">
-      				<h1>Your gainers</h1>   
-      				<canvas id="gainers" width="300" height="200"></canvas>     	
-
-      				<h1>Your losers</h1>
-      				<canvas id="losers" width="300" height="200"></canvas>     	
-              	</div>
-              	</div>
-              		
-          </div>
-        </div>
+            <div id="dashboard">
+            	<div id="networth">
+            		<h1>Net Worth</h1>
+            		<h2>Today</h2>
+            		<div>{this.state.portfolio.total}</div>
+            		<div className="networth-table">
+            			<div>This Month</div>
+            			<div>{thisMonthProfitLoss}</div>
+            			<div>{thisMonthPercentage}</div>
+            		</div>
+            		<div className="networth-table">
+	        			<div>YTD</div>
+	        			<div>{thisYearProfitLoss}</div>
+	        			<div>{thisYearPercentage}</div>
+	        		</div>
+            	</div>
+            </div>
+         </div>
         <Footer />
+        <Modal showing={this.state.timeout} allowNavigation={true}>
+	    	<h1>Could not contact Chelmer</h1>
+	    	<p>The Chelmer portal is currently down, so we are unable to retrieve your portfolio information.  Please try again later.</p>
+	    </Modal>
       </div>
     );
   }
--- src/components/Functions.js
+++ src/components/Functions.js
@@ -13,4 +13,47 @@ const ToAPIDate = function(date) {
 	return year + "-" + month + "-" + day;
 }
 
-export { ToAPIDate };
+const PortfolioFromHoldings = function(data) {
+	var theGroups = [];
+	var loadedPortfolio = {"parts":[], total:0};
+	for (var i=0; i<data.data.length; i++) {
+		var datachunk = data.data[i];
+		loadedPortfolio.date = new Date(datachunk.asAtDate);
+		
+		for (var j=0; j<datachunk.holdings.length; j++) {
+			var holding = datachunk.holdings[j];
+			var group = holding.mkt;
+			
+			var theGroup = theGroups[group];
+			if (theGroup == null) {
+				theGroup = {"name": group, "holdings":[], "amount":0, "expanded": false};
+				theGroups[group] = theGroup;
+			}
+			
+			theGroup.holdings.push({"name": holding.name, value: holding.baseMv});
+			theGroup.amount += holding.baseMv;
+			
+			loadedPortfolio.total += holding.baseMv;
+		}
+	}
+
+	for (var key in theGroups) {
+		loadedPortfolio.parts.push(theGroups[key]);
+	};
+
+	//Percentages
+	var maxPercentage = 0;
+	loadedPortfolio.parts.forEach(function(el) {
+			var perc = (el.amount * 100 / loadedPortfolio.total );
+			if (perc > maxPercentage) {
+				maxPercentage = perc;
+			}
+			el.percentage = perc;
+		});
+	loadedPortfolio.parts.forEach(function(el) {
+		el.maxPercentage = maxPercentage;
+		});
+	return loadedPortfolio;
+
+}
+export { ToAPIDate, PortfolioFromHoldings };
