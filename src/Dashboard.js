8ced765b1e4768de3959dd1da92465f131680323
Iain Matcham <iain@jumbletree.com>
2018-02-05 22:18:28 +1300

    Dashboard

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import Recharts, { PieChart, Pie, Cell, Legend, Sector } from 'recharts';
 import './App.css';
 
 import DateFormat from './components/DateFormat.js';
@@ -10,7 +10,7 @@ import Navigation from './components/Navigation.js';
 import Loading from './components/Loading.js';
 import Footer from './components/Footer.js';
 import Remote from './components/Remote.js';
-import { Portfolios } from './components/Constants.js';
+import { Portfolios, Palette, Markets } from './components/Constants.js';
 import { ToAPIDate, PortfolioFromHoldings } from './components/Functions.js';
 
 import './Dashboard.css';
@@ -23,7 +23,8 @@ class Dashboard extends Component {
 			date: new Date(),
 			parts:[]
 		},
-		timeout: false
+		timeout: false,
+		activeSegment: -1
 	};
 	
 	constructor(props) {
@@ -34,6 +35,8 @@ class Dashboard extends Component {
 		this.acceptPortfolio = this.acceptPortfolio.bind(this);
 		this.acceptMTDPerformance = this.acceptMTDPerformance.bind(this);
 		this.acceptYTDPerformance = this.acceptYTDPerformance.bind(this);
+		this.activateSegment = this.activateSegment.bind(this);
+		this.drawHighlight = this.drawHighlight.bind(this);
 		
 		this.load();
 	}
@@ -92,6 +95,40 @@ class Dashboard extends Component {
 		this.setState({"ytd": data.data[length-1]});
 	}
 	
+	activateSegment(data, index) {
+		this.setState({"activeSegment": index});
+	}
+	
+	drawHighlight(props) {
+		const { cx, cy, midAngle, innerRadius, outerRadius, startAngle, endAngle,
+		    fill, payload, percent, value, stroke } = props;
+		    
+		    
+		return (<g>
+			<Sector 
+				cx={cx}
+			    cy={cy}
+			    startAngle={startAngle}
+			    endAngle={endAngle}
+			    innerRadius={outerRadius + 6}
+			    outerRadius={outerRadius + 10}
+			    fill={fill} />
+			<Sector 
+				cx={cx}
+			    cy={cy}
+			    startAngle={startAngle}
+			    endAngle={endAngle}
+			    innerRadius={innerRadius}
+			    outerRadius={outerRadius}
+			    fill={fill}
+				stroke={stroke}
+			/>
+			<text x={30} y={210} textAnchor="left" fill={fill} style={{"fontSize": "16px"}}>{payload.name}</text>
+			<text x={30} y={230} textAnchor="left" fill="white" ><NumberFormat value={payload.percentage} places={1} suffix="%" /> of portfolio</text>
+			<text x={30} y={250} textAnchor="left" fill="white" >Current value <NumberFormat value={payload.holdingValue} places={2} prefix="$" /></text>
+		</g>);
+	}
+	
   render() {
 	  const wait = (<i className="fas fa-circle-notch fa-spin"></i>);
 	  
@@ -117,7 +154,32 @@ class Dashboard extends Component {
 		  thisYearProfitLoss = (<NumberFormat value={change} places={2} prefix="$" explicitPositive={true} />);
 		  thisYearPercentage = (<NumberFormat value={change/this.state.ytd.grossStartValue*100} places={2} suffix="%" explicitPositive={true} />);
 	  }
-
+	  
+	  const pieData = [];
+	  const outerPie = [];
+	  
+	  this.state.portfolio.parts.map((entry, index)=>{
+		 const colour = Palette[index % Palette.length];
+		 
+		 pieData.push({
+			 name: Markets[entry.name],
+			 percentage: entry.percentage,
+			 value: entry.total,
+			 colour: colour
+		 }); 
+		 
+		 entry.holdings.map((holding, innerIndex)=>{
+			 outerPie.push({
+				 name: holding.name,
+				 percentage: holding.percentage,
+				 "holdingValue": holding.value,
+				 colour: colour
+			 });
+			 return null;
+		 });
+		 return null;
+	  });
+	  
 	 return (
       <div className="App">
         <Navigation url={this.props.match.url} />
@@ -133,7 +195,7 @@ class Dashboard extends Component {
             	<div id="networth">
             		<h1>Net Worth</h1>
             		<h2>Today</h2>
-            		<div>{this.state.portfolio.total}</div>
+            		<div><NumberFormat places={2} value={this.state.portfolio.total} prefix="$" /></div>
             		<div className="networth-table">
             			<div>This Month</div>
             			<div>{thisMonthProfitLoss}</div>
@@ -144,6 +206,21 @@ class Dashboard extends Component {
 	        			<div>{thisYearProfitLoss}</div>
 	        			<div>{thisYearPercentage}</div>
 	        		</div>
+            	</div>
+            	<div id="assetgraph">
+            		<PieChart width={400} height={300}>
+            			<Pie data={pieData} nameKey="name" dataKey="percentage" cx="50%" cy="30%" startAngle={450} endAngle={90} innerRadius={50} outerRadius={60} legendType="square">
+            				{ pieData.map((entry, index)=>(
+            					<Cell key={"cell-" + index}	fill={entry.colour} />
+            				))}
+            			</Pie>
+            			<Pie data={outerPie} nameKey="name" dataKey="percentage" cx="50%" cy="30%" startAngle={450} endAngle={90} innerRadius={70} outerRadius={80} legendType="none" onMouseEnter={this.activateSegment} activeIndex={this.state.activeSegment} activeShape={this.drawHighlight}>
+	        				{ outerPie.map((entry, index)=>(
+	        					<Cell key={"ocell-" + index} fill={entry.colour} />
+	        				))}
+	        			</Pie>
+        				<Legend layout="vertical" align="right" verticalAlign="top"/>
+            		</PieChart>
             	</div>
             </div>
          </div>
